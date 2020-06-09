7c1c29fa7d2abf0758f4de5b86ab53b57a3b7995
Adam ONeil <adamosoftware@gmail.com>
2020-06-09 09:53:33 -0400

    removed validation code -- would require new dependency

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -11,7 +11,7 @@ namespace Models
     /// <summary>
     /// defines an activity such as Design, Develop, Test, Deploy
     /// </summary>
-    public class Activity : BaseTable, IValidate
+    public class Activity : BaseTable
     {
         [Key]
         [References(typeof(Workspace))]
@@ -23,25 +23,5 @@ namespace Models
 
         [References(typeof(Activity))]
         public int? NextActivityId { get; set; }
-
-        public ValidateResult Validate()
-        {
-            return new ValidateResult() { IsValid = true };
-        }
-
-        public async Task<ValidateResult> ValidateAsync(IDbConnection connection, IDbTransaction txn = null)
-        {
-            var circular = await IsCircularAsync(connection, Id, NextActivityId);
-            return (circular) ? 
-                new ValidateResult() { IsValid = false, Message = "Activity contains circular reference to next activity." } :
-                new ValidateResult() { IsValid = true };
-        }
-
-        private static Task<bool> IsCircularAsync(IDbConnection connection, int id, int? nextId)
-        {
-            if (!nextId.HasValue) return Task.FromResult(false);
-
-            throw new NotImplementedException();
-        }
     }
 }
