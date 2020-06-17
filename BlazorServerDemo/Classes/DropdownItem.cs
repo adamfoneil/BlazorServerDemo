57a0319fe20be9d546b8bfb43b21be2c9fbc2d6e
Adam ONeil <adamosoftware@gmail.com>
2020-06-17 10:56:22 -0400

    Iteration end day binding working

---
+++ BlazorServerDemo/Classes/DropdownItem.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorServerDemo.Classes
+{
+    public class DropdownItem<T>
+    {
+        public DropdownItem(T value, string text)
+        {
+            Value = value;
+            Text = text;
+        }
+
+        public T Value { get; }
+        public string Text { get; }
+    }
+}
